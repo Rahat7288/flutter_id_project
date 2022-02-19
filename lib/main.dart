90cfcf85e592287bf86afce2170cf9d76124a8ab
Rahat <rahatislamakash@gmail.com>
2022-02-20 01:08:41 +0600

    simple id card design with flutter for learning

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,80 @@ class Card extends StatelessWidget {
         backgroundColor: Colors.purple,
         elevation: 0.0,
       ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/cv.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.white,
+            ),
+            Text(
+              'Name:',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            Text(
+              'Rahat Islam Akash',
+              style: TextStyle(
+                color: Colors.black87,
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            Text(
+              'Id:',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            Text(
+              '1631239042',
+              style: TextStyle(
+                color: Colors.black87,
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 10.0,),
+                Text(
+                  'rahatislamakash@gmail.com',
+
+                ),
+              ],
+            ),
+
+
+
+          ],
+        ),
+      ),
     );
   }
 }
